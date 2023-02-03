d38ff83f2062e006eb42a3dc44c066183d57d9a6
MartinKonecny100128860 <100128860@student.burnley.ac.uk>
2023-02-03 10:21:59 +0000

    Added my name and current date.

--- ConsoleAppProject/Program.cs
+++ ConsoleAppProject/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleAppProject
     /// to start App01 to App05 for CO453 CW1
     /// 
     /// This Project has been modified by:
-    /// Derek Peacock 05/02/2022
+    /// Martin Konecny 03/02/2023
     /// </summary>
     public static class Program
     {
