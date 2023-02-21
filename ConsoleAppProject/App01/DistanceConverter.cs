c6a47f4fd2e2affe003ef7e543bc6c9ef96cdec5
MartinKonecny100128860 <100128860@student.burnley.ac.uk>
2023-02-21 20:14:56 +0000

    Added meters to the app as well as extra functions

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -12,9 +12,13 @@ namespace ConsoleAppProject.App01
     {
         public const int FEET_IN_MILES = 5280;
 
+        public const double METERS_IN_MILES = 1609.34;
+
         private double miles;
 
         private double feet;
+
+        private double meters;
 
         /// <summary>
         /// 
@@ -27,12 +31,31 @@ namespace ConsoleAppProject.App01
             OutputFeet();
         }
 
-        private void OutputHeading()
+        public void MilesToFeet()
         {
-            Console.WriteLine("\n-------------------------------------");
-            Console.WriteLine("       Convert Miles to Feet         ");
-            Console.WriteLine("            By Martin Konecny        ");
-            Console.WriteLine("-------------------------------------\n");
+            OutputHeading();
+            InputMiles();
+            CalculateFeet();
+            OutputFeet();
+
+        }
+
+        public void FeetToMiles()
+        {
+            OutputHeading();
+            InputMiles();
+            CalculateFeet();
+            OutputFeet();
+
+        }
+
+        public void MilesToMeters()
+        {
+            OutputHeading();
+            InputMiles();
+            CalculateFeet();
+            OutputFeet();
+
         }
 
         /// <summary>
@@ -42,19 +65,82 @@ namespace ConsoleAppProject.App01
         {
             Console.Write("Please enter the number of miles you want to convert ");
             string value = Console.ReadLine();
+            meters = Convert.ToDouble(value);
+
+        }
+
+        private void InputFeet()
+        {
+            Console.Write("Please enter the number of feet you want to convert ");
+            string value = Console.ReadLine();
+            feet = Convert.ToDouble(value);
+
+        }
+
+        private void InputMeters()
+        {
+            Console.Write("Please enter the number of meters you want to convert ");
+            string value = Console.ReadLine();
             miles = Convert.ToDouble(value);
 
         }
         /// <summary>
-        /// Converts miles to feet
+        /// Converts feet to miles (multiply)
         /// </summary>
         private void CalculateFeet()
         {
-            feet = miles * 5280;
+            feet = miles * FEET_IN_MILES;
         }
+
+        /// <summary>
+        /// Converts miles to feet (divide)
+        /// </summary>
+        private void CalculateMiles()
+        {
+            miles = feet / FEET_IN_MILES;
+        }
+
+        /// <summary>
+        /// Converts miles to feet (divide)
+        /// </summary>
+        private void CalculateMeters()
+        {
+            meters = miles * METERS_IN_MILES;
+        }
+
+        /// <summary>
+        /// Outputs feet to miles
+        /// </summary>
         private void OutputFeet()
         {
-            Console.WriteLine(miles + " miles is " + feet + " feet !");
+            Console.WriteLine(miles + " miles is " + feet + " feet! ");
+        }
+
+        /// <summary>
+        /// Outputs miles to feet
+        /// </summary>
+        private void OutputMiles()
+        {
+            Console.WriteLine(feet + " feet is " + miles + " miles! ");
+        }
+
+        /// <summary>
+        /// Outputs miles to meters
+        /// </summary>
+        private void OutputMeters()
+        {
+            Console.WriteLine(miles + " miles is " + meters + " meters! ");
+        }
+
+        /// <summary>
+        /// This is the heading which greets the user, summarizes the app and shows the author
+        /// </summary>
+        private void OutputHeading()
+        {
+            Console.WriteLine("\n-------------------------------------");
+            Console.WriteLine("       Convert Miles to Feet         ");
+            Console.WriteLine("            By Martin Konecny        ");
+            Console.WriteLine("-------------------------------------\n");
         }
     }
 }
