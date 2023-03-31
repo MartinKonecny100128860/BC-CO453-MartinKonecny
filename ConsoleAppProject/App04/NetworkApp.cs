75328aab2754ffde2c88c968bff389b9f85187a9
MartinKonecny100128860 <100128860@student.burnley.ac.uk>
2023-03-31 21:56:47 +0100

    Starting of Network App

---
+++ ConsoleAppProject/App04/NetworkApp.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppProject.App04
+{
+    internal class NetworkApp
+    {
+        private NewsFeed news = new NewsFeed();
+        public void Run()
+        {
+            throw new NotImplementedException();
+        }
+        private void PostMessage()
+        {
+            throw new NotImplementedException();
+        }
+        private void PostImage()
+        {
+            throw new NotImplementedException();
+        }
+        private void RemovePost()
+        {
+            throw new NotImplementedException();
+        }
+        private void DisplayAll()
+        {
+            throw new NotImplementedException();
+        }
+        private void DisplayByAuthor()
+        {
+            throw new NotImplementedException();
+        }
+        private void DisplayByDate()
+        {
+            throw new NotImplementedException();
+        }
+        private void AddComment()
+        {
+            throw new NotImplementedException();
+        }
+        private void LikePosts()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
