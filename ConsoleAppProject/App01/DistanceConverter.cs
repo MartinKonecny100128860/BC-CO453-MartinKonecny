0d48e0ad14b35966a0e6f629ca8a17f72db3ae94
MartinKonecny100128860 <100128860@student.burnley.ac.uk>
2023-04-05 23:38:09 +0100

    test

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -10,6 +10,7 @@ namespace ConsoleAppProject.App01
     /// This app will ask the user to enter ther number of miles (fromUnit) and convert it to feet or
     /// meters (toUnit) and it will also ask the user for the number of feet and convert it into miles.
     /// </summary>
+    /// 
     /// <author>
     /// Martin Konecny Ver. 4.1
     /// </author>
