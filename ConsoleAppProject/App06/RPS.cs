ea681b1559f50686967e2feedc99ad52083eee71
MartinKonecny100128860 <100128860@student.burnley.ac.uk>
2023-05-13 23:23:49 +0100

    Added comments to RPS game

--- ConsoleAppProject/App06/RPS.cs
+++ ConsoleAppProject/App06/RPS.cs
@@ -3,32 +3,50 @@ using ConsoleAppProject.Helpers;
 
 namespace ConsoleAppProject.App06
 {
+    /// <summary>
+    /// This app will ask the user to enter ther number of miles (fromUnit) and convert it to feet or
+    /// meters (toUnit) and it will also ask the user for the number of feet and convert it into miles.
+
+    /// </summary>
+    /// 
+    /// <author>
+    /// Martin Konecny Ver. 2.0
+    /// </author>
     internal class RPS
     {
         public void Main()
         {
+            // A loop which will keep the game running until the player decides to quit.
             do
             {
+                // object to generate random number
                 Random random = new Random();
                 int yourScore = 0;
                 int npcScore = 0;
 
+                // Outputs the game title using console helpers 
                 ConsoleHelper.OutputHeading("R-P-S Console Game!");
                 Console.ForegroundColor = ConsoleColor.DarkYellow;
 
+                // Propmpts the user to enter their name
                 Console.Write("Please enter your name: ");
                 string playerName = Console.ReadLine();
 
+                // loop for 3 rounds
                 while (yourScore != 3 && npcScore != 3)
                 {
                     Console.WriteLine(); // Prints a blank line
-                    Console.WriteLine("Your Score: " + yourScore + ". NPC Score: " + npcScore);
+                    Console.WriteLine("Your Score: " + yourScore + ". NPC Score: " + npcScore); // Prints score
+
+                    // Prompts the user to enter a letter for selection of rock, papper and scissors
                     Console.WriteLine("Please enter | 'R' for Rock | 'P' for Paper | or anything other for Scissors|");
                     string playerChoice = Console.ReadLine().ToLower();
 
+                    // generates random number to make a selection for the computer
                     int enemyChoice = random.Next(0, 3);
 
-                    if (enemyChoice == 0) // Enemy chose rock
+                    // switch statement to determine the winner of the round
+                    if (enemyChoice == 0) // NPC chose rock
                     {
                         Console.WriteLine("NPC chooses Rock! ");
 
@@ -89,6 +107,7 @@ namespace ConsoleAppProject.App06
 
                 }
 
+                // displays the final score
                 if (yourScore == 3)
                 {
                     Console.WriteLine(playerName + ", You win!");
@@ -98,6 +117,7 @@ namespace ConsoleAppProject.App06
                     Console.WriteLine(playerName + ", You lose!");
                 }
 
+                // prompts the user to play again or exit the game
                 Console.WriteLine();
                 Console.WriteLine("Do you want to play again? (Y/N)");
                 string playAgainChoice = Console.ReadLine().ToLower();
